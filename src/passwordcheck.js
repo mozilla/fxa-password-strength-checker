f744cd612f9d9f12695a38aaa1ca3e799032b60e
Sai Pc <schand31@asu.edu>
2015-07-27 12:40:48 -0700

    refactor(src): change variable and function name to better reflect meaning add bower to package.json escape from eslints clutches edit travis.yml

--- src/passwordcheck.js
+++ src/passwordcheck.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-// A ux utility to suggest password strength
+// A utility to check and log password strength
 
 define(['jquery',
   'bloomfilter'
@@ -11,14 +11,14 @@ function ($, bloomfilter) {
   'use strict';
 
   var NO_OF_HASHING_FUNCTIONS = 8;
-  var bloomfilter = null;
+  var bloom = null;
   $.ajax({
     method: 'GET',
     dataType: 'json',
     url: '../bower_components/fxa-password-strength-checker/src/bloomdata_short_pwd.js',
     error: function (jqxhr, status, err) { console.log(status + ' ERROR ' + err); }
   }).done(function (res){
-    bloomfilter = new BloomFilter(res, NO_OF_HASHING_FUNCTIONS); //eslint-disable-line no-undef
+    bloom = new BloomFilter(res, NO_OF_HASHING_FUNCTIONS); //eslint-disable-line no-undef
   });
 
   function strengthCheck(password) {
@@ -28,28 +28,28 @@ function ($, bloomfilter) {
     return regex.test(password);
   }
 
-  return function checkPasswordStrength(password, callback) {
+  return function(password, callback) {
     if (password.length < 8) {
       return;
     }
-    var bool = false;
-    var strong = strengthCheck(password.toString());
+    var isPasswordWeak = false;
+    var strong = strengthCheck(password);
 
     if (strong) {
       // Only if the password has a chance of being strong do we check with the bloom filter
       // else, simply reject the password. This helps us to not store all-alpha or all-numeric passwords
       // on the bloom filter, reducing space.
-      bool = bloomfilter.test(password);
+      isPasswordWeak = bloom.test(password);
       callback('Password strength check with bloomfilter triggered');
     } else {
-      bool = true;
+      isPasswordWeak = true;
       callback('Password with all alphabets/numbers detected');
     }
 
-    if (bool && strong) {
+    if (isPasswordWeak && strong) {
       // password was found in the bloom filter, or was too weak.
       callback('Password found in bloom filter');
-    } else if (!bool && strong) {
+    } else if (!isPasswordWeak && strong) {
       callback('Password was not found in bloom filter');
     }
   };
--- tests/bloomfilter_test.js
+++ tests/bloomfilter_test.js
@@ -1,17 +1,18 @@
 (function(){
+  /*global chai, bloom */
   'use strict';
-  var assert = chai.assert; //eslint-disable-line no-undef
+  var assert = chai.assert;
   describe('Bloomfilter', function(){
     var goodPasswords = ['notinyourdictionary!', 'imaynotbetheretoo', 'thisisagoodcleanpassword', 'combo1234$#@'];
     for(var goodPass in goodPasswords) {
       it('returns false when password is not in bloom filter', function(){
-        assert.isFalse(bloom.test(goodPasswords[goodPass])); //eslint-disable-line no-undef
+        assert.isFalse(bloom.test(goodPasswords[goodPass]));
       });
     }
     var badPasswords = ['hooters1', 'charlie2', '!ILO9VEYOU', 'hate_you1990'];
     for(var badPass in badPasswords) {
       it('returns true when password is in bloomfilter', function () {
-        assert.isTrue(bloom.test(badPasswords[badPass])); //eslint-disable-line no-undef
+        assert.isTrue(bloom.test(badPasswords[badPass]));
       });
     }
   });
