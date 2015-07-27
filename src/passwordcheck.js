dec817892c5b19a0ee44cee2a0813ef9cdd677c9
Sai Pc <schand31@asu.edu>
2015-07-27 11:27:37 -0700

    refactor passwordcheck to take in password and a callback

--- src/passwordcheck.js
+++ src/passwordcheck.js
@@ -28,33 +28,27 @@ function ($, bloomfilter) {
     return regex.test(password);
   }
 
-  return function checkPasswordStrength(view, target, metrics, translator) {
-    var element = $(target);
-    if (! element.length) {
-      return;
-    }
-
-    var password = target.val();
-    var strong = false;
-    var bool = false;
+  return function checkPasswordStrength(password, callback) {
     if (password.length < 8) {
       return;
     }
-    strong = strengthCheck(password.toString());
+    var bool = false;
+    var strong = strengthCheck(password.toString());
+
     if (strong) {
       // Only if the password has a chance of being strong do we check with the bloom filter
       // else, simply reject the password. This helps us to not store all-alpha or all-numeric passwords
       // on the bloom filter, reducing space.
       bool = bloomfilter.test(password);
-      metrics.logEvent('Password strength check with bloomfilter triggered');
+      callback('Password strength check with bloomfilter triggered');
     } else {
       bool = true;
-      metrics.logEvent('Password with all alphabets/numbers detected');
+      callback('Password with all alphabets/numbers detected');
     }
 
     if (bool) {
       // password was found in the bloom filter, or was too weak.
-      metrics.logEvent('Password found in bloom filter or was weak');
+      callback('Password found in bloom filter or was weak');
     }
   };
 });
