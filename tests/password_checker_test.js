4b5692bfa254986ed6066203df5b8b651e03a05d
Sai Pc <schand31@asu.edu>
2015-08-03 11:05:26 -0700

    chore(copyright): add copyright info

--- tests/password_checker_test.js
+++ tests/password_checker_test.js
@@ -1,6 +1,7 @@
-/**
- * Created by schandramouli on 7/29/15.
- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 (function() {
   /*global chai, passwordcheck */
   'use strict';
